9cab444d7989d684ddf2a05aac98f36008b17db9
Heath Nail <sitedyno@gmail.com>
2017-05-03 00:18:58 -0400

    Initial plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,9 +10,11 @@
 
 namespace Sitedyno\PhergieFortune;
 
+use DomainException;
 use Phergie\Irc\Bot\React\AbstractPlugin;
 use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
 use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
+use React\ChildProcess\Process;
 
 /**
  * Plugin class.
@@ -23,17 +25,117 @@ use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
 class Plugin extends AbstractPlugin
 {
     /**
+     * Path to the fortune binary
+     *
+     * @var string
+     */
+    protected $binaryPath = '/usr/games/fortune';
+
+    /**
+     * List of fortune databases to use.
+     *
+     * @var string
+     */
+    protected $databases = 'fortunes';
+
+    /**
+     * Invalid binary path.
+     *
+     * @var int
+     */
+    const INVALID_BINARY_PATH = 1;
+
+    /**
+     * Invalid databases value.
+     *
+     * @var int
+     */
+    const INVALID_DATABASES = 2;
+
+    /**
+     * Invalid short value.
+     *
+     * @var int
+     */
+    const INVALID_SHORT = 3;
+
+    /**
+     * True to use short fortunes (-s). False to use any length fortunes.
+     *
+     * @var bool
+     */
+    protected $short = true;
+
+    /**
      * Accepts plugin configuration.
      *
      * Supported keys:
      *
+     * binary-path - The path to the fortune binary. Defaults to '/usr/games/fortune'.
      *
+     * databases - List of databases to use. Defaults to 'fortunes'. See `man fortune` for possible values.
+     *
+     * short - Use only short fortunes. Defaults to true.
      *
      * @param array $config
      */
     public function __construct(array $config = [])
     {
+        if (isset($config['binary-path'])) {
+            $this->setBinaryPath($config['binary-path']);
+        }
+        if (isset($config['databases'])) {
+            $this->setDatabases($config['databases']);
+        }
+        if (isset($config['short'])) {
+            $this->setShort($config['short']);
+        }
+    }
 
+    /**
+     * Gets the path to the fortune binary.
+     *
+     * @return string
+     */
+    public function getBinaryPath()
+    {
+        return $this->binaryPath;
+    }
+
+    /**
+     * Gets the databases to use for the fortune command.
+     *
+     * @return string
+     */
+    public function getDatabases()
+    {
+        return $this->databases;
+    }
+
+    /**
+     * Gets the fortune command.
+     *
+     * @return string
+     */
+    public function getFortuneCommand()
+    {
+        if ($this->short) {
+            $short = ' -s';
+        } else {
+            $short = null;
+        }
+        $cmd = $this->binaryPath . $short . " " . $this->databases;
+        return $cmd;
+    }
+
+    /**
+     * Gets the value for short.
+     *
+     * @return bool
+     */
+    public function getShort()
+    {
+        return $this->short;
     }
 
     /**
@@ -56,5 +158,68 @@ class Plugin extends AbstractPlugin
      */
     public function handleFortune(Event $event, Queue $queue)
     {
+        $fortune = new Process($this->getFortuneCommand());
+        $fortune->on('exit', function($exitCode, $termSignal) {
+            if (0 !== $exitCode) {
+                $cmd = $this->getFortuneCommand();
+                $this->logger->error("$cmd exited with exit code: $exitCode");
+            }
+        });
+        $fortune->start($this->getLoop());
+        $fortune->stdout->on('data', function ($chunk) use ($event, $queue) {
+            $chunk = str_replace("\n", " ", $chunk);
+            $queue->ircPrivmsg(
+                $event->getSource(),
+                $event->getNick() . ": " . $chunk
+            );
+        });
+    }
+
+    /**
+     * Sets the path to the fortune binary.
+     *
+     * @return void
+     */
+    public function setBinaryPath($path)
+    {
+        if (!is_string($path)) {
+            throw new DomainException(
+                'binary-path must be a string',
+                Plugin::INVALID_BINARY_PATH
+            );
+        }
+        $this->binaryPath = $path;
+    }
+
+    /**
+     * Sets the databases to use for the fortune command.
+     *
+     * @return void
+     */
+    public function setDatabases($databases)
+    {
+        if (!is_string($databases)) {
+            throw new DomainException(
+                'databases must be a string',
+                Plugin::INVALID_DATABASES
+            );
+        }
+        $this->databases = $databases;
+    }
+
+    /**
+     * Sets the short value.
+     *
+     * @return void
+     */
+    public function setShort($short)
+    {
+        if (!is_bool($short)) {
+            throw new DomainException(
+                'short must be true or false',
+                Plugin::INVALID_SHORT
+            );
+        }
+        $this->short = $short;
     }
 }
--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Phergie plugin for Display a fortune in channel (https://github.com/sitedyno/phergie-fortune)
+ * Phergie plugin for displayng a random fortune in channel (https://github.com/sitedyno/phergie-fortune)
  *
  * @link https://github.com/sitedyno/phergie-fortune for the canonical source repository
  * @copyright Copyright (c) 2017 Heath Nail (https://github.com/sitedyno)
@@ -10,6 +10,7 @@
 
 namespace Sitedyno\PhergieFortune\Tests;
 
+use DomainException;
 use Phake;
 use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
 use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
@@ -23,7 +24,58 @@ use Sitedyno\PhergieFortune\Plugin;
  */
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Configurations for testGetFortuneCommand()
+     */
+    public function getFortuneCommandConfigurations()
+    {
+        yield [
+            ['short' => false],
+            '/usr/games/fortune fortunes'
+        ];
 
+        yield [
+            ['databases' => '10% cookie fortunes'],
+            '/usr/games/fortune -s 10% cookie fortunes'
+        ];
+    }
+
+    /**
+     * Data provider for testInvalidConfigurations()
+     */
+    public function invalidConfigurations()
+    {
+        yield [
+            ['binary-path' => false],
+            Plugin::INVALID_BINARY_PATH
+        ];
+
+        yield [
+            ['databases' => false],
+            Plugin::INVALID_DATABASES
+        ];
+
+        yield [
+            ['short' => 'not boolean'],
+            Plugin::INVALID_SHORT
+        ];
+    }
+
+    /**
+     * Tests getFortuneCommand()
+     *
+     * @param array $config
+     * @param string $expected
+     * @dataProvider getFortuneCommandConfigurations
+     */
+    public function testGetFortuneCommand($config, $expected)
+    {
+        $plugin = new Plugin($config);
+
+        $result = $plugin->getFortuneCommand();
+
+        $this->assertSame($expected, $result);
+    }
 
     /**
      * Tests that getSubscribedEvents() returns an array.
@@ -33,4 +85,115 @@ class PluginTest extends \PHPUnit_Framework_TestCase
         $plugin = new Plugin;
         $this->assertInternalType('array', $plugin->getSubscribedEvents());
     }
+
+    /**
+     * Tests that handleFortune() will log an error with non-zero exit code.
+     */
+    public function testHandleFortuneNonZeroExitCode()
+    {
+        $event = Phake::mock('\Phergie\Irc\Plugin\React\Command\CommandEvent');
+        $queue = Phake::mock('\Phergie\Irc\Bot\React\EventQueueInterface');
+        $logger = Phake::mock('\Monolog\Logger');
+        $loop = \React\EventLoop\Factory::create();
+
+        $plugin = new Plugin(['binary-path' => 'not_existing_binary']);
+        $plugin->setLogger($logger);
+        $plugin->setLoop($loop);
+
+
+        $plugin->handleFortune($event, $queue);
+
+        $loop->run();
+
+        Phake::verify($logger)->error("not_existing_binary -s fortunes exited with exit code: 127");
+    }
+
+    /**
+     * Tests handleFortune() queues a message on success.
+     */
+    public function testHandleFortuneQueuesMessageOnSuccess()
+    {
+        $event = Phake::mock('\Phergie\Irc\Plugin\React\Command\CommandEvent');
+        $queue = Phake::mock('\Phergie\Irc\Bot\React\EventQueueInterface');
+        $loop = \React\EventLoop\Factory::create();
+        $fortune = "You will find a rewarding development job soon!";
+
+        Phake::when($event)->getSource()->thenReturn('#sitedyno');
+        Phake::when($event)->getNick()->thenReturn('sitedyno');
+
+        $plugin = new Plugin([
+            'binary-path' => 'echo',
+            'databases' => $fortune,
+            'short' => false
+        ]);
+        $plugin->setLoop($loop);
+
+        $plugin->handleFortune($event, $queue);
+
+        $loop->run();
+
+        Phake::verify($queue)->ircPrivmsg(
+            '#sitedyno',
+            "sitedyno: $fortune "
+        );
+    }
+
+    /**
+     * Tests that an exception is thrown for invalid configurations.
+     *
+     * @param array $config
+     * @param int $error
+     * @dataProvider invalidConfigurations
+     */
+    public function testInvalidConfigurations(array $config, $error)
+    {
+        try {
+            $plugin = new Plugin($config);
+        } catch (DomainException $e) {
+            $this->assertSame($error, $e->getCode());
+        }
+    }
+
+    /**
+     * Tests that SetBinaryPath() accepts a string.
+     */
+    public function testSetBinaryPathAcceptsString()
+    {
+        $plugin = new Plugin;
+        $path = '/some/path';
+
+        $plugin->setBinaryPath($path);
+        $result = $plugin->getBinaryPath();
+
+        $this->assertSame($path, $result);
+    }
+
+    /**
+     * Tests that setDatabases() accepts a string.
+     */
+    public function testSetDatabasesAcceptsString()
+    {
+        $plugin = new Plugin;
+        $databases = 'startrek';
+
+        $plugin->setDatabases($databases);
+        $result = $plugin->getDatabases();
+
+        $this->assertSame($databases, $result);
+    }
+
+    /**
+     * Tests that setShort() accepts false.
+     */
+    public function testSetShortAcceptsFalse()
+    {
+        $plugin = new Plugin;
+        $expected = false;
+
+        $plugin->setShort($expected);
+        $result = $plugin->getShort();
+
+        $this->assertSame($expected, $result);
+    }
+
 }
